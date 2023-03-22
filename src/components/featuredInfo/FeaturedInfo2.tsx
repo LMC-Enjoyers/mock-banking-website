4f7a17dcbd85f6614a8b25ae2e902223ba1f1595
Shee69 <lehoangnamtep@gmail.com>
2023-03-22 23:52:57 +0000

    Fix position of cards

--- src/components/featuredInfo/FeaturedInfo2.tsx
+++ src/components/featuredInfo/FeaturedInfo2.tsx
@@ -3,17 +3,15 @@ import "./featuredInfo.css";
 
 export default function FeaturedInfo2() {
   return (
-    <div className="flex flex-row h-full w-full flex-wrap">
+    <div className="flex flex-row h-full w-full">
       <div className="flex flex-col flex-grow">
-        <div className="flex h-1/2 items-end justify-center mb-1">
-          <div className="flex h-5/6 w-5/6 bg-bank-black border rounded-3xl shadow"></div>
-        </div>
-        <div className="flex h-1/2 items-start justify-center mt-1">
-          <div className="flex h-5/6 w-5/6 bg-white border rounded-3xl shadow"></div>
+        <div className="flex flex-col h-full">
+          <div className="flex mt-8 ml-28 h-2/5 w-5/6  bg-bank-black border rounded-3xl shadow"></div>
+          <div className="flex mt-6 ml-28 h-2/5 w-5/6 bg-white border rounded-3xl shadow"></div>
         </div>
       </div>
-      <div className="flex flex-grow items-start mt-28 justify-center">
-        <div className="flex h-5/6 w-3/4 bg-white border rounded-3xl shadow"></div>
+      <div className="flex flex-grow items-start justify-end">
+        <div className="flex mt-8 mr-28 h-5/6 w-5/6 bg-white border rounded-3xl shadow"></div>
       </div>
     </div>
   );
