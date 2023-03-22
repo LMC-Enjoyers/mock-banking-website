cc1f53308015c7c59d62b562d1c0c80b91b5641c
Shee69 <lehoangnamtep@gmail.com>
2023-03-22 17:15:34 +0000

    Add some responsive feature

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,11 +3,10 @@ import "./sidebar.css";
 import { BsFillGrid1X2Fill } from "react-icons/bs";
 import { BsBarChartSteps } from "react-icons/bs";
 import { BiLogOut } from "react-icons/bi";
-import { fontWeight } from "@mui/system";
 
 export default function Sidebar() {
   return (
-    <div className="fixed top-0 left-0 h-screen w-64 flex flex-col bg-bank-black">
+    <div className="fixed top-0 left-0 h-screen w-48 lg:w-64 flex flex-col bg-bank-black">
       <div className="flex flex-col w-4/5 h-4/5 self-center mt-28">
         <SidebarStart icon={<BsFillGrid1X2Fill color="white" size='20'/>} item={"Overview"} />
         <SidebarItem icon={<BsBarChartSteps color="white" size='20'/>} item={"Summary"} />
