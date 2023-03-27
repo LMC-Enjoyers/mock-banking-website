5f16ee27f52b54ea5937b6c340816407055db49d
Michal Pluta <michalpl2003@gmail.com>
2023-03-27 16:32:46 +0100

    Access database using typeOrm

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,26 +1,28 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/user.entity"
+import { Account } from "./entity/account.entity"
+import { Branch } from "./entity/branch.entity"
+import { AccountType } from "./entity/account_type.entity"
+import { Transaction } from "./entity/transaction.entity"
+import { TransactionCategory } from "./entity/transaction_category.entity"
 
-const PATH_TO_DATABASE = 'bank.db';
+const PATH_TO_DATABASE = './src/db/bank.db';
 
-async function connectToDatabase() {
-  const db = await open({
-    filename: PATH_TO_DATABASE,
-    driver: sqlite3.Database
-  });
-  return db;
-}
-
-async function getAllUsers() {
-    try {
-        const db = await connectToDatabase();
-  
-        const rows = await db.all('SELECT * FROM tblUser');
-      
-        return rows;
-        
-    } catch (error: any) {
-        console.error(error.message);
-    }
-
-  }
+export const AppDataSource = new DataSource({
+        type: 'sqlite',
+        database: PATH_TO_DATABASE,
+        entities: [User, Account, Branch, AccountType, Transaction, TransactionCategory],
+        synchronize: true,
+    })
+    
+AppDataSource.initialize()
+      .then(async () => {
+          console.log("Data Source has been initialized!")
+          const userRepository = AppDataSource.getRepository(User);
+          const rows = await userRepository.find();
+          console.log(rows)
+      })
+      .catch((err) => {
+          console.error("Error during Data Source initialization", err)
+      })
