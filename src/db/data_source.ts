c056b4ee9b5c2262d61b4f932b543c1eeca8d82e
Michal Pluta <michalpl2003@gmail.com>
2023-03-27 16:48:40 +0100

    Separate data source and controller functionality

--- src/db/data_source.ts
+++ src/db/data_source.ts
@@ -15,14 +15,3 @@ export const AppDataSource = new DataSource({
         entities: [User, Account, Branch, AccountType, Transaction, TransactionCategory],
         synchronize: true,
     })
-    
-AppDataSource.initialize()
-      .then(async () => {
-          console.log("Data Source has been initialized!")
-          const userRepository = AppDataSource.getRepository(User);
-          const rows = await userRepository.find();
-          console.log(rows)
-      })
-      .catch((err) => {
-          console.error("Error during Data Source initialization", err)
-      })
