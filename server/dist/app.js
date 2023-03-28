7612148ee2c336cf90ea040cd0d43aea15aa6ab9
Jeremy Mariani <hrwv51@durham.ac.uk>
2023-03-28 18:58:41 +0100

    remove unecessary noce_modules

--- server/dist/app.js
+++ server/dist/app.js
@@ -4,5 +4,6 @@ const express = require('express');
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "src/App.tsx")));
+app.listen(5050);
 exports.default = app;
 //# sourceMappingURL=app.js.map
