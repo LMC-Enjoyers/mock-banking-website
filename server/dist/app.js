863c8dd2a445d25c03de6ecd868c39c170f3fe6b
Shee69 <lehoangnamtep@gmail.com>
2023-03-31 23:45:03 +0100

    Revert "Revert "Merge pull request #20 from JoshisonGitHub:currentBranch""
    This reverts commit 455c01ed8ef30533308e6a57edad954f90113495.

--- server/dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const path = require("path");
-const app = express();
-app.use(express.static(path.join(__dirname, "src/App.tsx")));
-app.listen(5050);
-exports.default = app;
-//# sourceMappingURL=app.js.map
