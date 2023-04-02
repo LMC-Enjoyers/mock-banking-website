b5a3b7ead8e1b092d55a57f0ca11671e6476a722
Michal Pluta <michalpl2003@gmail.com>
2023-04-02 17:43:12 +0100

    Use the new implementation in index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,15 +3,17 @@ import { Branch } from "./entity/branch.entity";
 
 async function test() {
 
-    const branch = await BranchController.getByID('4b9fc2ad-f9d6-4f08-9757-9f505019518d')
+
+    const bc = new BranchController()
+    const branch = await bc.getByID('4b9fc2ad-f9d6-4f08-9757-9f505019518d')
     console.log("Found branch: ", branch)
 
     const newBranch = new Branch()
     newBranch.branch_name = "Natwest Durham"
     newBranch.branch_sort_code = "398259"
-    await BranchController.insert(newBranch)
+    await bc.insert(newBranch)
 
-    const branches = await BranchController.getAll();
+    const branches = await bc.getAll();
     console.log("Loaded branches: ", branches)
 };
 
