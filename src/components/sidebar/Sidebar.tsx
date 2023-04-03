29638bd02bb09ab28462741354689bbdb4c14aa4
Shee69 <lehoangnamtep@gmail.com>
2023-04-03 15:36:00 +0100

    Change content

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import CreateAccountModal from "./modals/Modals";
 import { BsFillGrid1X2Fill } from "react-icons/bs";
-import { BsBarChartSteps } from "react-icons/bs";
 import { BiLogOut } from "react-icons/bi";
-import { AiFillBank } from "react-icons/ai";
+import CreateAccount from "./modals/Modals";
+import { SwitchAccount } from "./modals/Modals";
 import { TransferMoney } from "./modals/Modals";
 import { DeleteAccount } from "./modals/Modals";
 import { AddFunds } from "./modals/Modals";
@@ -11,10 +10,11 @@ import { AddFunds } from "./modals/Modals";
 
 export default function Sidebar() {
   return (
-    <div className="flex flex-col top-0 left-0 h-auto w-0 sm:w-32 md:w-48 bg-bank-black overflow-auto">
+    <div className="flex flex-col top-0 left-0 h-auto w-0 sm:w-36 md:w-48 bg-bank-black overflow-auto">
       <div className="flex flex-col w-4/5 h-4/5 self-center mt-32">
         <SidebarItem icon={<BsFillGrid1X2Fill color="white" size='20'/>} item={"Overview"} content={"Overview"}/>
-        <CreateAccountModal/>
+        <CreateAccount/>
+        <SwitchAccount/>
         <DeleteAccount/>
         <AddFunds/>
         <TransferMoney/>
@@ -49,70 +49,3 @@ const SidebarEnd = ({ icon, item }: { icon: any, item: string }) => (
   </button>
 
 )
-
-/*
-const SidebarDropDown = () => (
-  <div className="flex flex-col gap-4">
-    <div className="flex items-center gap-4">
-      <Dropdown
-        label="Dropdown right start"
-        placement="right-start"
-      >
-        <Dropdown.Item>
-          Dashboard
-        </Dropdown.Item>
-        <Dropdown.Item>
-          Settings
-        </Dropdown.Item>
-        <Dropdown.Item>
-          Earnings
-        </Dropdown.Item>
-        <Dropdown.Item>
-          Sign out
-        </Dropdown.Item>
-      </Dropdown>
-    </div>
-  </div>
-)
-*/
-function AccountModalcontent (){
-  return (
-    <div id="Account" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
-      <div className="relative w-full h-full max-w-md md:h-auto">
-          {/*<!-- Modal content -->*/}
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-              <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="Account">
-                  <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                  <span className="sr-only">Close modal</span>
-              </button>
-              <div className="px-6 py-6 lg:px-8">
-                  <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
-                  <form className="space-y-6" action="#">
-                      <div>
-                          <label htmlFor= "email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                          <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required></input>
-                      </div>
-                      <div>
-                          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                          <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required></input>
-                      </div>
-                      <div className="flex justify-between">
-                          <div className="flex items-start">
-                              <div className="flex items-center h-5">
-                                  <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required></input>
-                              </div>
-                              <label htmlFor = "remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                          </div>
-                          <a href="#" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-                      </div>
-                      <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                      <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                          Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
-                      </div>
-                  </form>
-              </div>
-          </div>
-      </div>
-    </div> 
-  )
-}
--- src/components/sidebar/modals/Modals.tsx
+++ src/components/sidebar/modals/Modals.tsx
@@ -6,7 +6,7 @@ import { AiFillRest } from "react-icons/ai";
 
 import{ useState } from 'react';
 
-export default function CreateAccountModal() {
+export default function CreateAccount() {
   const [showModal, setShowModal] = React.useState(false);
 
   /*
@@ -116,19 +116,17 @@ export default function CreateAccountModal() {
       ) : null}
     </>
   );
+
 }
 
-
-
-
-export function TransferMoney() {
+export function SwitchAccount() {
   const [showModal, setShowModal] = React.useState(false);
   return (
     <>
       <button onClick={() => setShowModal(true)} className="sidebar-item" type="button">
-        <AiFillCreditCard color="white" size='20'/>
+        <AiFillRest color="white" size='20'/>
         <div className="text-white font-medium p-4">
-          Transfer Money
+          Switch Account
         </div>
       </button>
       {showModal ? (
@@ -142,7 +140,7 @@ export function TransferMoney() {
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">
-                    Transfer Money
+                    Delete Account
                   </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
@@ -155,11 +153,11 @@ export function TransferMoney() {
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <form id="TransferMoney">
+                  <form id="DeleteAccount">
                     <div className="grid gap-6 mb-6 md:grid-cols-2">
                     </div>
                     <div className="mb-6">
-                        <label htmlFor="account_type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">To</label>
+                        <label htmlFor="account_type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Account</label>
                         <select id="account_type" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                           <option selected>Current</option>
                           <option value="CA">Savings</option>
@@ -168,27 +166,13 @@ export function TransferMoney() {
                           <option value="DE">Student</option>
                         </select>
                       </div>
-                    <div className="mb-6">
-                        <label htmlFor="account_type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">From</label>
-                        <select id="account_type" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                          <option selected>Current</option>
-                          <option value="CA">Savings</option>
-                          <option value="FR">Business</option>
-                          <option value="DE">Child</option>
-                          <option value="DE">Student</option>
-                        </select>
-                    </div>
-                    <div className="mb-6">
-                      <label htmlFor="account name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Amount</label>
-                      <input type="Number" id="account name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="100" required></input>
-                    </div> 
                     <div className="flex items-start mb-6">
                       <div className="flex items-center h-5">
                         <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required></input>
                       </div>
-                      <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-black-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
+                      <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-black-300">Are you sure you want to delete this account?</label>
                     </div>
-                    <button type="submit" className="text-white bg-emerald-500 hover:bg-emerald-600 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">Transfer</button>
+                    <button type="submit" className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete</button>
                   </form>
                 </div>
                 {/*footer*/}
@@ -211,8 +195,6 @@ export function TransferMoney() {
     </>
   );
 }
-
-
 
 export function DeleteAccount() {
   const [showModal, setShowModal] = React.useState(false);
@@ -349,6 +331,122 @@ export function AddFunds() {
                       </div>
                       <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-black-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
                     </div>
+                    <button type="submit" className="text-white bg-emerald-500 hover:bg-emerald-600 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">Transfer</button>
+                  </form>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
+
+interface TransferValues {
+  to: string;
+  from: string;
+  amount: number;
+}
+
+export function TransferMoney() {
+  const [showModal, setShowModal] = React.useState(false);
+
+  const [values, setValues] = useState<TransferValues>({ to: '', from: '', amount: 0 });
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      console.log("hi")
+      const response = await fetch('/user_details', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    setValues((prevValues) => ({ ...prevValues, [name]: value }));
+  };
+  return (
+    <>
+      <button onClick={() => setShowModal(true)} className="sidebar-item" type="button">
+        <AiFillCreditCard color="white" size='20'/>
+        <div className="text-white font-medium p-4">
+          Transfer Money
+        </div>
+      </button>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Transfer Money
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <form onSubmit={handleSubmit}>
+                    <div className="grid gap-6 mb-6 md:grid-cols-2">
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="account_type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">To</label>
+                        <select name="from" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                          <option selected value="" disabled>Current</option>
+                          <option value="CA">Savings</option>
+                          <option value="FR">Business</option>
+                          <option value="DE">Child</option>
+                          <option value="DE">Student</option>
+                        </select>
+                      </div>
+                    <div className="mb-6">
+                        <label htmlFor="account_type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">From</label>
+                        <select name="to" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                          <option selected value="" disabled>Current</option>
+                          <option value="CA">Savings</option>
+                          <option value="FR">Business</option>
+                          <option value="DE">Child</option>
+                          <option value="DE">Student</option>
+                        </select>
+                    </div>
+                    <div className="mb-6">
+                      <label htmlFor="account name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Amount</label>
+                      <input onChange={handleChange} name="amount" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="100" required></input>
+                    </div> 
                     <button type="submit" className="text-white bg-emerald-500 hover:bg-emerald-600 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">Transfer</button>
                   </form>
                 </div>
